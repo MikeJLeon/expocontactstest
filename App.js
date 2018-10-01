2177abbdb3c1c82d5c16e72bb37691fa62c8119c
Michael Leon <michaeljleon@live.com>
2018-09-30 21:53:14 -0700

    Attempting to add sectionlist
    Attempting to add sectionlist

--- App.js
+++ App.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, FlatList, ListItem} from 'react-native';
+import { TouchableOpacity, SectionList, StyleSheet, Text, View, Image, FlatList} from 'react-native';
 import { Contacts, Permissions } from 'expo';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      dataSections:[],
       dataContacts:[]
     };
-    //Get phone contacts
-    this.getContacts();
   }
-  async getContacts(){
+  async componentDidMount() {
     const permission = await Permissions.askAsync(Permissions.CONTACTS);
     //Check permission status
     if (permission.status !== 'granted') {
@@ -29,29 +28,71 @@ export default class App extends React.Component {
         ]
       });
       //Print contacts total
-      console.log(data.total);
+      //console.log(data.total);
       //Check if at least 1 contact is available
+      const sections = {};
       if (data.total > 0) {
         //Grab contact name
-        const contact = data.data[0].name;
         //Print contact name in log
-        console.log(data);
-        console.log(data.data[0].phoneNumbers);
-        console.log(data.data[0].phoneNumbers[0].number)
+        for (var prop in data.data) {
+          var name = (data.data[prop].name);
+          var fL = name.charAt(0);          
+          if(!(fL in sections)){
+            sections[fL] = new Array();
+            sections[fL].push(name);
+          }else{
+            sections[fL].push(name); 
+          }
+        }
+        this.setState({dataSections: sections})
         this.setState({dataContacts: data.data});
-        
       }
     }
-  }
+  }  
+
   render() {
+    const _renderItem = ({item, section}) => (<Text>{`${item.name}(${section.key})`}</Text>)
+
+    const _renderSectionHeader = ({section}) => {
+        return (
+          <View style={styles.sectionHeader}>
+            <Text style={styles.header}>{section.key}</Text>
+          </View>
+        )
+    }
+  /*   const _renderItem = ({item}) => {
+      if(!item.imageAvailable){
+         return (
+            <View style={styles.contacts}>
+              <Image style={styles.image} source={{uri: 'https://s3.eu-west-2.amazonaws.com/soundwise/uploads/user-profile/default.png'}}/>
+              <Text style={styles.name} key={item.id}>{item.name}</Text>
+              <TouchableOpacity style={styles.button}>
+              </TouchableOpacity>
+            </View>
+            )
+      }else{
+        return (
+          <View style={styles.contacts}>
+            <Image style={styles.image} source={{uri: item.image.uri}}/>
+            <Text style={styles.name} key={item.id}>{item.name}</Text>
+            <TouchableOpacity style={styles.button}>
+            </TouchableOpacity>
+          </View>
+        )
+      }
+    } */
     return (
       <View style={styles.container}>
         <Text>First line</Text>
-        <FlatList
+        {/* <FlatList
           data={this.state.dataContacts}
-          renderItem={({item}) => 
-            <Text>{item.name}, {item.phoneNumbers[0].number}</Text>
-          }
+          renderItem={_renderItem}
+          keyExtractor={item => item.id}
+        /> */}
+        <SectionList
+            sections={this.state.dataSections}
+            renderItem={_renderItem}
+            renderSectionHeader={_renderSectionHeader}
         />
       </View>
     );
@@ -60,9 +101,29 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  contacts:{
+    flex:1, 
+    flexDirection:"row",
+    justifyContent:"space-between"
+  },
+  image:{
+    margin:10,
+    width:50,
+    height:50,
+  },
+  name:{
+    padding:10,
+    position:'absolute',
+    left:60,
+    top:15,
+  },
+  button:{
+    marginTop:25,
+    marginRight:10,
+    width:60,
+    height:25,
+    backgroundColor:"orange",
+  }
 });
